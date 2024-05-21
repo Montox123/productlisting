698c5ed0a68985f6591da6b79a8bdbffa8a26564
Montox123 <60232414+Montox123@users.noreply.github.com>
2024-05-21 13:01:31 +0530

    second commit

--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -1,7 +1,7 @@
 export function fetchAllProducts() {
   return new Promise(async (resolve) =>{
     //TODO: we will not hard-code server URL here
-    const response = await fetch('http://localhost:8080/products') 
+    const response = await fetch('http://localhost:8000/products') 
     const data = await response.json()
     resolve({data})
   }
@@ -18,7 +18,7 @@ export function fetchProductsByFilters(filter) {
 
   return new Promise(async (resolve) =>{
     //TODO: we will not hard-code server URL here
-    const response = await fetch('http://localhost:8080/products?'+queryString) 
+    const response = await fetch('http://localhost:8000/products?'+queryString) 
     const data = await response.json()
     resolve({data})
   }
